d769267b88f651c8f9b689939935d4b3a136eb3a
cassiusbessa <cassiusbessa@gmail.com>
2023-03-23 17:31:49 -0300

    feat: :sparkles: logger operations

--- handlers/create-service.go
+++ handlers/create-service.go
@@ -4,24 +4,31 @@ import (
 	"net/http"
 
 	"github.com/cassiusbessa/create-service/entities"
+	"github.com/cassiusbessa/create-service/logs"
 	"github.com/cassiusbessa/create-service/repositories"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 func CreateService(c *gin.Context) {
 	var service entities.Service
+	db := c.Param("company")
+	logrus.Warnf("Creating Service on %v", db)
 	if err := c.BindJSON(&service); err != nil {
+		logrus.Errorf("Error decoding Service %v: %v", db, err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
 		return
 	}
 	if err := service.Validate(); err != nil {
+		logrus.Errorf("Error validating Service on %v: %v", db, err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": err})
 		return
 	}
-	db := c.Param("company")
 	if err := repositories.CreateService(db, service); err != nil {
+		logrus.Errorf("Error Inserting Service to MongoDB: %v %v", db, err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	defer logs.Elapsed("Create Service")()
 	c.JSON(http.StatusCreated, gin.H{"message": "Service created successfully"})
 }
--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"github.com/cassiusbessa/create-service/handlers"
+	"github.com/cassiusbessa/create-service/logs"
 	"github.com/cassiusbessa/create-service/repositories"
 	"github.com/sirupsen/logrus"
 )
 
+var file = logs.Init()
+
 func main() {
+	defer file.Close()
 	r := handlers.Router()
 	repositories.Repo.Ping()
 	r.POST("/services/:company", handlers.CreateService)
+	r.StaticFile("/logs", "./logs/logs.log")
 	if err := r.Run(":8080"); err != nil {
 		logrus.Fatalf("Failed to start server: %v", err)
 	}
